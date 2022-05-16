76917ecfb20615e10ebe78c9442255f6060bafe7
armando-michalski <armando.rial.michalski@gmail.com>
2022-05-16 19:01:57 -0300

    Aula10 - 09/05/22 - Classes abstratas

--- Aula09/Carro.py
+++ Aula09/Carro.py
@@ -6,18 +6,22 @@ class Carro( Veiculo ):
 		super().__init__(modelo, ano)
 		self.portas = portas
 
+	#METODO ACESSOR (GETTER)
 	@property
 	def modelo(self):
 		return self.__modelo
 
+	#MÉTODO MODIFICADOR (SETTER)
 	@modelo.setter 
 	def modelo(self, valor):
 		self.__modelo = valor
 
+	#METODO ACESSOR (GETTER)
 	@property
 	def ano(self):
 		return self.__ano
 
+	#MÉTODO MODIFICADOR (SETTER)
 	@ano.setter 
 	def ano(self, valor):
 		self.__ano = valor
--- Aula09/Veiculo.py
+++ Aula09/Veiculo.py
@@ -4,31 +4,37 @@ from abc import ABCMeta, abstractmethod
 class Veiculo( metaclass=ABCMeta ):
 
 	def __init__(self, modelo, ano = None):
-		self.__modelo = modelo
+		self.__modelo = modelo #__ significa fortemente privado, ou seja não pode utilizar o atributo modelo, sem passar pela property
 		self.__ano = ano
 
+	#METODO ACESSOR (GETTER)
 	@property
 	def modelo(self):
 		pass
 
+	#MÉTODO MODIFICADOR (SETTER)
 	@modelo.setter
-	@abstractmethod
+	@abstractmethod #Obrigatoriamente deve ser reimplementado
 	def modelo( self, valor):
 		pass
 
+	#METODO ACESSOR (GETTER)
 	@property
 	def ano(self):
 		pass
-
+	
+	#MÉTODO MODIFICADOR (SETTER)
 	@ano.setter
 	@abstractmethod
 	def ano( self, valor):
 		pass
 
+	#MÉTODO NÃO-ABSTRATO = PODE SER RE-ESCRITO
 	def imprimir(self):
 		print("Modelo: ", self.__modelo)
 		print("Ano: ", self.__ano)
 
+	#MÉTODO ABSTRATO = DEVE SER RE-ESCRITO
 	@abstractmethod
 	def imprimirEspecifico(self):
 		pass
